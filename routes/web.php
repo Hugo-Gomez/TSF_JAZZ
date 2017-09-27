6f352882b9e6b0f8d07aaa9d5cf87ad592b7cabe
Manu Marchand <manumarchand@MC-G-LP-C120-7.local>
2017-09-27 18:41:15 +0200

    Updating database structure + adding agenda with multiple events + PHP route at /fetch_events to fetch, update and store new events in the database

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,45 @@ Route::get('about', function () {
 Route::get('contact', function () {
     return view('contact');
 });
+
+
+
+/* DATABASE DEV 
+*
+*
+*
+*/
+Route::get('fetch_events', function () {
+    $html = file_get_contents('http://www.tsfjazz.com/web_service/agenda.php?days=20');
+    $xml = simplexml_load_string($html, null, LIBXML_NOCDATA);
+ 
+    foreach($xml->Agenda as $agenda){
+
+        if (!DB::table('agenda')->where('id',(string)$agenda->id)->value('title')){
+
+            foreach($agenda->children() as $key => $value){
+                if ( !in_array($key, array('mail','picture'), true ))
+                    $array[$key] = (string)$value;
+            }
+    
+            $explode = explode("/", $array['date']);
+            $day   = $explode[0];
+            $month = $explode[1];
+            $year  = $explode[2];
+    
+            if (strlen($month) == 1)
+                $month = '0'.$month; 
+    
+            $date = new DateTime($day.'-'.$month.'-'.$year);
+            $array['date'] = $date->format('Y-m-d');
+            $array['display'] = $array['une'];
+            $array['zipcode'] = $array['zip'];
+            unset($array['une']);
+            unset($array['zip']);
+            
+            DB::table('agenda')->insert([$array]);
+            $array = null;
+        }
+    }
+    return "done";
+});
