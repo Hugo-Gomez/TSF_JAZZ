6368138eb2f2b0a07afba3c21af9cbbb8393de25
Hugo Gomez <hugo.gomez9795@gmail.com>
2017-09-28 16:44:21 +0200

    Setup of the Jazzblog page

--- resources/views/structure.blade.php
+++ resources/views/structure.blade.php
@@ -7,6 +7,13 @@
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="xmlrpc.php">
 
+  
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
   <title>
     LP JAZZ
   </title>
