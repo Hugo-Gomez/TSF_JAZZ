a5e9ae523a179496020c59e88cb7b2885117212e
Raphael Renout <renout@MC-G-LP-C120-15.local>
2017-09-28 17:41:10 +0200

    Modification of the StructureController for news feed in the structure blade

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -13,47 +13,58 @@ class StructureController extends Controller
 	}
 
 	function program() {
-    	return view('program');
+    $news = DB::table('news')->get();
+    	return view('program', compact('news'));
 	}
 
 	function jazzenda() {
-    	return view('jazzenda');
+    $news = DB::table('news')->get();
+    	return view('jazzenda', compact('news'));
 	}
 
 	function podcast() {
-    	return view('podcast');
+    $news = DB::table('news')->get();
+    	return view('podcast', compact('news'));
 	}
 
 	function jazzblog() {
+    $news = DB::table('news')->get();
 		$blog = DB::table('blog')->get();
-    	return view('jazzblog', compact('blog'));
+    	return view('jazzblog', compact('blog'), compact('news'));
 	}
 
 	function audtre() {
-    	return view('audtre');
+    $news = DB::table('news')->get();
+    	return view('audtre', compact('news'));
 	}
 
 	function trackhist() {
-    	return view('trackhist');
+    $news = DB::table('news')->get();
+    	return view('trackhist', compact('news'));
 	}
 
 	function media() {
-    	return view('media');
+    $news = DB::table('news')->get();
+    	return view('media', compact('news'));
 	}
 
 	function about() {
+    $news = DB::table('news')->get();
     	return view('about');
 	}
 
 	function contact() {
-    	return view('contact');
+    $news = DB::table('news')->get();
+    	return view('contact', compact('news'));
 	}
 
   function frequences() {
-    	return view('frequences');
+    $news = DB::table('news')->get();
+    	return view('frequences', compact('news'));
 	}
 
   function newsletter() {
-    	return view('newsletter');
+    $news = DB::table('news')->get();
+    	return view('newsletter', compact('news'));
 	}
 }
