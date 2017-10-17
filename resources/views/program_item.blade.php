060570231d04f0ac6f85531b9b075d3902738f72
Hugo Gomez <hugo.gomez9795@gmail.com>
2017-10-17 11:53:07 +0200

    Program page with all the specification and shows for each programs

--- resources/views/program_item.blade.php
+++ resources/views/program_item.blade.php
@@ -7,6 +7,7 @@
 @section('stylesheet')
 <link rel="stylesheet" type="text/css" href="../public/css/program.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 @endsection
 
 @section('contenu')
@@ -19,6 +20,8 @@
                 </div>
                 <div class="col m9">
                     <p class="program-title">{{ $program->title }}</p>
+                    <h1>{{ $program->hebdo_date }}</h1>
+                    <br>
                     <p>{{ $program->description }}</p>
                 </div>
             </div>
@@ -27,10 +30,16 @@
 
     <div class="separator"></div>
 
-    <ul class="program-days">
-        @foreach ($program_items as $program_item)
-            <li>Emission du <a href="">{{ date('d/m/Y', strtotime($program_item->date)) }}</a></li>
-        @endforeach
-    </ul>
+    @foreach ($program_items as $program_item)
+        <div class="program-event">
+            <h1 class="progevent-date">Emission du {{ date('d/m/Y', strtotime($program_item->date)) }}</h1>
+            <br>
+            <h3>{{ $program_item->subtitle }}</h3>
+            <br>
+            <p>{{ $program_item->description }}</p>
+            <br>
+        </div>
+        <div class="separator progevent"></div>
+    @endforeach
 
 @endsection
