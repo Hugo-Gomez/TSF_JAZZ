a50a92779fdaac8811c533164d1ddb79db53696c
Hugo Gomez <hugo.gomez9795@gmail.com>
2017-10-19 15:23:09 +0200

    Add the 404 not found page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<body>
+	<div class="container">
+		<img src="../../../public/img/news1.png">
+		<h1>La page à laquelle vous essayez d'accéder est introuvable (erreur: 404)</h1>
+		<a href="{{ url('/') }}">Retourner sur la page d'accueil</a>
+	</div>
+</body>
+
+<style type="text/css">
+
+	body {
+		text-align: center;
+		background-color: #eaeaea;
+		font-family: "Titillium", Arial, sans-serif;
+	}
+
+	.container {
+		margin-top: 15%;
+	}
+
+	h1 {
+		color: #731818;
+	}
+
+</style>
